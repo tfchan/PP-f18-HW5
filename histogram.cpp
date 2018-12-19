47504bb400cd465fb7b6032e52dd192f307a9644
tfchan <cseelemon@gmail.com>
2018-12-19 23:27:59 +0800

    Reformat the code

--- histogram.cpp
+++ histogram.cpp
@@ -5,34 +5,30 @@
 #include <iostream>
 
 unsigned int * histogram(unsigned int *image_data, unsigned int _size) {
-
 	unsigned int *img = image_data;
 	unsigned int *ref_histogram_results;
 	unsigned int *ptr;
 
 	ref_histogram_results = (unsigned int *)malloc(256 * 3 * sizeof(unsigned int));
 	ptr = ref_histogram_results;
-	memset (ref_histogram_results, 0x0, 256 * 3 * sizeof(unsigned int));
+	memset(ref_histogram_results, 0x0, 256 * 3 * sizeof(unsigned int));
 
 	// histogram of R
-	for (unsigned int i = 0; i < _size; i += 3)
-	{
+	for (unsigned int i = 0; i < _size; i += 3) {
 		unsigned int index = img[i];
 		ptr[index]++;
 	}
 
 	// histogram of G
 	ptr += 256;
-	for (unsigned int i = 1; i < _size; i += 3)
-	{
+	for (unsigned int i = 1; i < _size; i += 3) {
 		unsigned int index = img[i];
 		ptr[index]++;
 	}
 
 	// histogram of B
 	ptr += 256;
-	for (unsigned int i = 2; i < _size; i += 3)
-	{
+	for (unsigned int i = 2; i < _size; i += 3) {
 		unsigned int index = img[i];
 		ptr[index]++;
 	}
@@ -40,11 +36,9 @@ unsigned int * histogram(unsigned int *image_data, unsigned int _size) {
 	return ref_histogram_results;
 }
 
-int main(int argc, char const *argv[])
-{
-
-	unsigned int * histogram_results;
-	unsigned int i=0, a, input_size;
+int main(int argc, char const *argv[]) {
+	unsigned int *histogram_results;
+	unsigned int i = 0, a, input_size;
 	std::fstream inFile("input", std::ios_base::in);
 	std::ofstream outFile("xxxxxx.out", std::ios_base::out);
 
