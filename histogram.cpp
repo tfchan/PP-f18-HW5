38da9e4b23010d62cf7acf3b9c0b97cf17b776d4
tfchan <cseelemon@gmail.com>
2018-12-21 20:40:37 +0800

    Finish calculating histogram in kernel

--- histogram.cpp
+++ histogram.cpp
@@ -128,33 +128,9 @@ unsigned int * histogram(unsigned int *image_data, unsigned int _size) {
 		exit(EXIT_FAILURE);
 	}
 
-	unsigned int *img = image_data;
+	// Allocate space for result in host
 	unsigned int *ref_histogram_results;
-	unsigned int *ptr;
-
 	ref_histogram_results = (unsigned int *)malloc(256 * 3 * sizeof(unsigned int));
-	ptr = ref_histogram_results;
-	memset(ref_histogram_results, 0x0, 256 * 3 * sizeof(unsigned int));
-
-	// histogram of R
-	for (unsigned int i = 0; i < _size; i += 3) {
-		unsigned int index = img[i];
-		ptr[index]++;
-	}
-
-	// histogram of G
-	ptr += 256;
-	for (unsigned int i = 1; i < _size; i += 3) {
-		unsigned int index = img[i];
-		ptr[index]++;
-	}
-
-	// histogram of B
-	ptr += 256;
-	for (unsigned int i = 2; i < _size; i += 3) {
-		unsigned int index = img[i];
-		ptr[index]++;
-	}
 
 	// Read histogram result back from device to host after kernel event
 	error_num = clEnqueueReadBuffer(command_queue, device_histogram_result, CL_FALSE, 0, 256 * 3 * sizeof(unsigned int), ref_histogram_results, 1, &kernel_event, NULL);
